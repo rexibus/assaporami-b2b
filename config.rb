1ab473372a28d77d5e6ed36338e72ba38e6cc724
RegisWeill <regis_weill@hotmail.com>
2019-10-09 00:28:25 +0200

    middleman init

--- config.rb
+++ config.rb
@@ -1,49 +1,46 @@
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-activate :sprockets
+# Layouts
+# https://middlemanapp.com/basics/layouts/
 
+# Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-configure :build do
-  activate :minify_css
-  activate :minify_javascript
-  activate :asset_hash
-  activate :relative_assets
-  set :relative_links, true
-end
+# With alternative layout
+# page '/path/to/file.html', layout: 'other_layout'
 
-activate :deploy do |deploy|
-  deploy.build_before = true
-  deploy.deploy_method = :git
-end
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
 
-# module Rack
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
 
-#   class TryStatic
+# Helpers
+# Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
 
-#     def initialize(app, options)
-#       @app = app
-#       @try = ['', *options.delete(:try)]
-#       @static = ::Rack::Static.new(lambda { [404, {}, []] }, options)
-#     end
-
-#     def call(env)
-#       orig_path = env['PATH_INFO']
-#       found = nil
-#       @try.each do |path|
-#         resp = @static.call(env.merge!({'PATH_INFO' => orig_path + path}))
-#         break if 404 != resp[0] && found = resp
-#       end
-#       found or @app.call(env.merge!('PATH_INFO' => orig_path))
-#     end
+# helpers do
+#   def some_helper
+#     'Helping'
 #   end
 # end
 
-# use Rack::TryStatic, :root => "build", :urls => %w[/], :try => ['.html', 'index.html', '/index.html']
+# Build-specific configuration
+# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# Run your own Rack app here or use this one to serve 404 messages:
-
+# configure :build do
+#   activate :minify_css
+#   activate :minify_javascript
+# end
